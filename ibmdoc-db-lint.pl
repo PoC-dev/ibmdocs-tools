00c8d39e2622a7fc998dbc4292d4db70d97af5bd
Patrik Schindler <poc@pocnet.net>
2025-10-27 21:54:53 +0100

    Using hashes for speed improvement

--- ibmdoc-db-lint.pl
+++ ibmdoc-db-lint.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl -w
 
-# Copyright 2023 Patrik Schindler <poc@pocnet.net>
+# Copyright 2023, 2025 Patrik Schindler <poc@pocnet.net>
 #
 # This script is part of the IBM Documentation Utilities, to be found on https://github.com/PoC-dev/ibmdocs-tools - see there for
 # further details.
@@ -40,13 +40,15 @@ my $odbc_password = "myas400password";
 my $docpath = "/var/www/default/pages/ibmdocs";
 
 # Vars.
-my ($counter, $db_doc_count, $dbh, $db_typ_count, $dirfh, $docnbr, $doctype, $errcount, $file, $num_entries,
-    $odbc_delete_doctyp_sth, $odbc_list_alltypes_sth, $odbc_check_doc_sth, $odbc_check_doctyp_sth, $odbc_list_orphans_sth,
-    $odbc_insert_doc_sth, $odbc_insert_doctyp_sth, $title, $today, $tmpstr, @filelist
+my ($db_doc_count, $dbh, $db_typ_count, $dirfh, $docnbr, $doctype, $errcount, $ext, $file, $num_entries, %ibmdocpf_hash,
+    %ibmdoctypf_hash, $title, $today, $tmpstr, @filelist
 );
 
 # Causes the currently selected handle to be flushed immediately and after every print. Execute anytime before using <STDOUT>.
 $| = 1;
+
+$today = strftime('%Y-%m-%d', gmtime());
+$errcount = 0;
 
 #-----------------------------------------------------------------------------------------------------------------------------------
 # Connect, etc.
@@ -60,119 +62,107 @@ if ( ! defined($dbh) ) {
     printf(" OK.\n");
 }
 
-#-------------------------------------------------------------------------------
-# Look if a record in file system has an according entry in the database.
-printf("Comparing Directory to Database (this takes a while)...\n");
 
-# Prepare SQL statements.
-$odbc_check_doctyp_sth = $dbh->prepare("SELECT COUNT(*) FROM ibmdoctypf WHERE docnbr=? AND doctype=?");
+printf("Caching database contents...\n");
+my $odbc_load_docnbr_sth = $dbh->prepare("SELECT docnbr FROM ibmdocpf");
 if (defined($dbh->errstr)) {
-    printf("SQL preparation error for odbc_check_doctyp(): %s\n", $dbh->errstr);
+    printf("SQL preparation error for odbc_load_docnbr_sth(): %s\n", $dbh->errstr);
     die;
 }
-$odbc_insert_doctyp_sth = $dbh->prepare("INSERT INTO ibmdoctypf (docnbr, doctype, date_added) VALUES (?, ?, ?)");
+my $odbc_load_doctype_sth = $dbh->prepare("SELECT docnbr, doctype FROM ibmdoctypf");
 if (defined($dbh->errstr)) {
-    printf("SQL preparation error for odbc_insert_doctyp(): %s\n", $dbh->errstr);
-    die;
-}
-$odbc_check_doc_sth = $dbh->prepare("SELECT COUNT(*) FROM ibmdocpf WHERE docnbr=?");
-if (defined($dbh->errstr)) {
-    printf("SQL preparation error for odbc_check_doc(): %s\n", $dbh->errstr);
-    die;
-}
-$odbc_insert_doc_sth = $dbh->prepare("INSERT INTO ibmdocpf (docnbr) VALUES (?)");
-if (defined($dbh->errstr)) {
-    printf("SQL preparation error for odbc_insert_doc(): %s\n", $dbh->errstr);
+    printf("SQL preparation error for odbc_load_doctype_sth(): %s\n", $dbh->errstr);
     die;
 }
 
-$today = strftime('%Y-%m-%d', gmtime());
-$counter = 0;
-$errcount = 0;
+
+$odbc_load_docnbr_sth->execute;
+while ( ($docnbr) = $odbc_load_docnbr_sth->fetchrow_array ) {
+    $docnbr =~ s/\s+$//; # Remove trailing spaces
+    $ibmdocpf_hash{$docnbr} = 1;
+}
+
+$odbc_load_doctype_sth->execute();
+while ( ($docnbr, $doctype) = $odbc_load_doctype_sth->fetchrow_array ) {
+    $docnbr =~ s/\s+$//;
+    $doctype =~ s/\s+$//;
+    $ibmdoctypf_hash{"$docnbr|$doctype"} = 1;
+}
+
+
+# Clean up after ourselves.
+$odbc_load_docnbr_sth->finish;
+$odbc_load_doctype_sth->finish;
 
 #---------------------------------------
-# Get a list of directory entries into an array.
 
+printf("Generate a list of directory entries...\n");
 opendir($dirfh, $docpath);
 @filelist = readdir($dirfh);
 closedir($dirfh);
 @filelist = sort(@filelist);
 $num_entries = scalar(@filelist);
 
-#---------------------------------------
-# Wade through the file list and check database for an entry.
-# FIXME: This might be sped up considerably when caching the database tables locally.
+#-------------------------------------------------------------------------------
+printf("Phase 1: Comparing directory with cached database content...\n");
 
+# Prepare SQL statements.
+my $odbc_insert_doctyp_sth = $dbh->prepare("INSERT INTO ibmdoctypf (docnbr, doctype, date_added) VALUES (?, ?, ?)");
+if (defined($dbh->errstr)) {
+    printf("SQL preparation error for odbc_insert_doctyp(): %s\n", $dbh->errstr);
+    die;
+}
+my $odbc_insert_doc_sth = $dbh->prepare("INSERT INTO ibmdocpf (docnbr) VALUES (?)");
+if (defined($dbh->errstr)) {
+    printf("SQL preparation error for odbc_insert_doc(): %s\n", $dbh->errstr);
+    die;
+}
+
+#---------------------------------------
+
+# Wade through the file list and check database for an entry.
+# This is sped up considerably by earlier caching the database tables locally in hashes.
 foreach $file (@filelist) {
     if ( $file =~ /^(\S+)\.(pdf|boo)$/i ) {
-        $counter++;
-        printf("\r%d %%    ", ($counter / $num_entries * 100));
-
         # Dissect filename into document number and file type.
         $docnbr = $1;
-        $file =~ /^[^._]\S+\.(pdf|boo)$/i;
-        if ( lc($1) eq 'pdf' ) {
+        $ext = lc($2);
+        if ($ext eq 'pdf') {
             $doctype = 'P';
-        } elsif ( lc($1) eq 'boo' ) {
+        } elsif ($ext eq 'boo') {
             $doctype = 'B';
         } else {
-            printf("\nDon't know how to handle unknown file type for '%s'. Ignoring.\n\n", $file);
+            printf("Don't know how to handle unknown file type for '%s'. Ignoring.\n", $file);
             next;
         }
 
-        # Check database for an entry of a given $docnbr, $doctype.
-        $odbc_check_doctyp_sth->execute($docnbr, $doctype);
-        if (defined($dbh->errstr)) {
-            printf("\nSQL execution error at odbc_check_doctyp(): %s, skipping.\n\n", $dbh->errstr);
-            $errcount++;
-            next;
-        }
-
-        # How many records have we found?
-        ($db_typ_count) = $odbc_check_doctyp_sth->fetchrow;
-        if (defined($dbh->errstr)) {
-            printf("\nSQL fetch error at odbc_check_doctyp(): %s, skipping.\n\n", $dbh->errstr);
-            $errcount++;
-            next;
-        }
-
-        if ( $db_typ_count == 0 ) {
-            # There's no entry for given doctype.
-            printf("\nINSERT INTO ibmdoctypf (docnbr, doctype, date_added) VALUES ('%s', '%s', '%s');\n",
+        # Check local hash for ibmdoctypf entry
+        unless (exists $ibmdoctypf_hash{"$docnbr|$doctype"}) {
+            printf("INSERT INTO ibmdoctypf (docnbr, doctype, date_added) VALUES ('%s', '%s', '%s');\n",
                 $docnbr, $doctype, $today);
             $odbc_insert_doctyp_sth->execute($docnbr, $doctype, $today);
             if (defined($dbh->errstr)) {
-                printf("\nSQL execution error at odbc_insert_doctyp(): %s, skipping.\n\n", $dbh->errstr);
+                printf("SQL execution error at odbc_insert_doctyp(): %s, skipping.\n", $dbh->errstr);
                 $errcount++;
                 next;
             }
 
-            # Check if we have a associated record in ibmdocpf.
-            $odbc_check_doc_sth->execute($docnbr);
-            if (defined($dbh->errstr)) {
-                printf("\nSQL execution error at odbc_check_doc(): %s\n\n", $dbh->errstr);
-                $dbh->do("rollback");
-                $errcount++;
-                next;
-            }
+            # Update hash to reflect new entry
+            $ibmdoctypf_hash{"$docnbr|$doctype"} = 1;
 
-            ($db_doc_count) = $odbc_check_doc_sth->fetchrow;
-            if (defined($dbh->errstr)) {
-                printf("\nSQL fetch error at odbc_check_doc(): %s\n\n", $dbh->errstr);
-                $dbh->do("rollback");
-                $errcount++;
-                next;
-            }
-            if ( $db_doc_count == 0 ) {
-                # Insert a template entry.
-                printf("\nINSERT INTO ibmdocpf (docnbr) VALUES ('%s');\n", $docnbr);
+            # Check local hash for ibmdocpf entry
+            unless (exists $ibmdocpf_hash{$docnbr}) {
+                printf("INSERT INTO ibmdocpf (docnbr) VALUES ('%s');\n", $docnbr);
                 $odbc_insert_doc_sth->execute($docnbr);
                 if (defined($dbh->errstr)) {
-                    printf("\nSQL execution error at odbc_insert_doc(): %s\n\n", $dbh->errstr);
+                    printf("SQL execution error at odbc_insert_doc(): %s\n", $dbh->errstr);
                     $dbh->do("rollback");
                     $errcount++;
                     next;
                 }
+
+                # Update hash to reflect new entry
+                $ibmdocpf_hash{$docnbr} = 1;
             }
         }
     }
@@ -181,14 +171,8 @@ foreach $file (@filelist) {
 #---------------------------------------
 
 # Clean up after ourselves.
-if ( $odbc_check_doctyp_sth ) {
-    $odbc_check_doctyp_sth->finish;
-}
 if ( $odbc_insert_doctyp_sth ) {
     $odbc_insert_doctyp_sth->finish;
-}
-if ( $odbc_check_doc_sth ) {
-    $odbc_check_doc_sth->finish;
 }
 if ( $odbc_insert_doc_sth ) {
     $odbc_insert_doc_sth->finish;
@@ -198,23 +182,22 @@ if ( $odbc_insert_doc_sth ) {
 if ( $errcount eq 0 ) {
     # Writes to the database may now commence: There were no errors.
     $dbh->do("commit");
-    printf("\n");
 } else {
-    printf("\nEncountered %d errors underway. Issuing rollback() and exiting now. Please check.\n\n", $errcount);
+    printf("Encountered %d errors underway. Issuing rollback() and exiting now. Please check.\n", $errcount);
     $dbh->do("rollback");
     die;
 }
 
 #-------------------------------------------------------------------------------
 # See if we have a file for each database record.
-printf("Comparing Database to Directory...\n");
+printf("Phase 2: Comparing cached database content to directory...\n");
 
-$odbc_list_alltypes_sth = $dbh->prepare("SELECT docnbr, doctype FROM ibmdoctypf ORDER BY docnbr, doctype");
+my $odbc_list_alltypes_sth = $dbh->prepare("SELECT docnbr, doctype FROM ibmdoctypf ORDER BY docnbr, doctype");
 if (defined($dbh->errstr)) {
     printf("SQL preparation error for odbc_check_alltyp(): %s\n", $dbh->errstr);
     die;
 }
-$odbc_delete_doctyp_sth = $dbh->prepare("DELETE FROM ibmdoctypf WHERE docnbr=? AND doctype=?");
+my $odbc_delete_doctyp_sth = $dbh->prepare("DELETE FROM ibmdoctypf WHERE docnbr=? AND doctype=?");
 if (defined($dbh->errstr)) {
     printf("SQL preparation error for odbc_delete_doctyp(): %s\n", $dbh->errstr);
     die;
@@ -242,7 +225,7 @@ while( ($docnbr, $doctype) = $odbc_list_alltypes_sth->fetchrow) {
     $tmpstr = sprintf("%s/%s", $docpath, $docnbr);
 
     if ( $doctype eq 'P' && ! -e $tmpstr . '.pdf' ) {
-        printf("Found %s.pdf in DB but not in file system. Deleting entry.\n\n", $docnbr);
+        printf("Found %s.pdf in DB but not in file system. Deleting entry.\n", $docnbr);
         $odbc_delete_doctyp_sth->execute($docnbr, 'P');
     } elsif ( $doctype eq 'B' && ! -e $tmpstr . '.boo' ) {
         printf("Found %s.boo in DB but not in file system. Deleting entry.\n", $docnbr);
@@ -266,18 +249,17 @@ if ( $odbc_list_alltypes_sth ) {
 if ( $errcount eq 0 ) {
     # Writes to the database may now commence: There were no errors.
     $dbh->do("commit");
-    printf("\n");
 } else {
-    printf("\nEncountered %d errors underway. Issuing rollback() and exiting now. Please check.\n\n", $errcount);
+    printf("Encountered %d errors underway. Issuing rollback() and exiting now. Please check.\n", $errcount);
     $dbh->do("rollback");
     die;
 }
 
 #-------------------------------------------------------------------------------
 # Print a list of records in ibmdocpf with no corresponding doctype.
-printf("Checking for orphaned records in ibmdocpf (no entry in ibmdoctypf)...\n");
+printf("Phase 3: Checking for orphaned records in ibmdocpf (no entry in ibmdoctypf)...\n");
 
-$odbc_list_orphans_sth = $dbh->prepare("
+my $odbc_list_orphans_sth = $dbh->prepare("
     SELECT docnbr, title FROM ibmdocpf
       WHERE docnbr NOT IN
         (SELECT DISTINCT docnbr FROM ibmdoctypf)
@@ -318,16 +300,14 @@ if ( $odbc_list_orphans_sth ) {
 }
 
 # Handle errors gracefully. No commit/rollback, because we only SELECTed records.
-if ( $errcount eq 0 ) {
-    printf("\n");
-} else {
-    printf("\nEncountered %d errors underway. Exiting now. Please check.\n\n", $errcount);
+if ( $errcount gt 0 ) {
+    printf("Encountered %d errors underway. Exiting now. Please check.\n", $errcount);
     die;
 }
 
 #-------------------------------------------------------------------------------
 # Print a list of records in ibmdoctypf with no corresponding doc metadata.
-printf("Checking for orphaned records in ibmdoctypf (no entry in ibmdocpf)...\n");
+printf("Phase 4: Checking for orphaned records in ibmdoctypf (no entry in ibmdocpf)...\n");
 
 $odbc_list_orphans_sth = $dbh->prepare("
     SELECT DISTINCT docnbr FROM ibmdoctypf
@@ -369,10 +349,8 @@ if ( $odbc_list_orphans_sth ) {
 }
 
 # Handle errors gracefully. No commit/rollback, because we only SELECTed records.
-if ( $errcount eq 0 ) {
-    printf("\n");
-} else {
-    printf("\nEncountered %d errors underway. Exiting now. Please check.\n\n", $errcount);
+if ( $errcount gt 0 ) {
+    printf("Encountered %d errors underway. Exiting now. Please check.\n", $errcount);
     die;
 }
 
